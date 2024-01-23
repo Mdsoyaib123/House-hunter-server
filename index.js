be9c16a9372da125a6fa26e31241f3476f564adc
Mdsoyaib123 <mdsoyaibsourav@gmail.com>
2024-01-23 16:54:16 +0600

    authintication completed

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 require("dotenv").config();
+var jwt = require('jsonwebtoken');
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
@@ -31,12 +32,63 @@ async function run() {
     // collection
 
     const userCollection = client.db("House-hunters").collection("users");
+    
+    
+    app.post('/jwt',async(req,res)=>{
+      const user = req.body 
+      // console.log('user',user);
+      const token = jwt.sign(user , process.env.ACCESS_TOKEN_SECRET,{expiresIn:'24h'})
+      res.send(token) 
+      
+     })
+
+     app.post('/checkCurrentUser',async(req,res,next)=>{
+      const localToken = req.body.token  
+      if(!localToken){
+        return res.send({message:'please login '})
+      }
+      jwt.verify(localToken,process.env.ACCESS_TOKEN_SECRET,(error,decoded)=>{
+        if(error){
+          return res.status(401).send({ message: "unAuthorized access" });
+        }
+        // console.log('decoded token ',decoded);
+        req.user = decoded
+        
+      })
+      const email =  req.user.email
+      const query = {email:email}
+      const result = await userCollection.findOne(query)
+      res.send(result)
+      
+     })
+
+     app.post('/LoginToken',async(req,res)=>{
+      const LoginTokenData = req.body 
+      const email = LoginTokenData.email 
+      const password = LoginTokenData.password 
+      const query = {email:email}
+      const LoginData = await userCollection.findOne(query)
+      if(LoginData){
+        const token = jwt.sign(LoginTokenData , process.env.ACCESS_TOKEN_SECRET,{expiresIn:'24h'})
+
+      res.send(token)
+
+      }
+     })
 
     app.post("/users", async (req, res) => {
       const userData = req.body;
+      const email = userData.email
+      const query = {email:email}
+      const existingUser =await userCollection.findOne(query);
+      if(existingUser){
+        return res.send({ message: "email already exist", insertedId: null });
+      }
       const result = await userCollection.insertOne(userData);
       res.send(result);
     });
+
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
